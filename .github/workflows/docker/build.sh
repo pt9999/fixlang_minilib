0e5acb3523f972f6d72cda8b0d08919da16534bd
pt9999 <18054486+pt9999@users.noreply.github.com>
2024-01-19 21:22:15 +0900

    update docker/build.sh

--- .github/workflows/docker/build.sh
+++ .github/workflows/docker/build.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+set -ex
+
+IMAGE=pt9999/fix
+DATE=$(date +%Y%m%d)
+
 mkdir -p .local
 cp -pui ~/.cargo/bin/fix .local/fix
-docker build . -t pt9999/fix:latest
+docker build . -t ${IMAGE}:latest
+docker tag ${IMAGE}:latest ${IMAGE}:${DATE}

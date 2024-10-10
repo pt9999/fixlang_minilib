7a578e5a772e1f8f907ab62080f364f0df573fc8
pt9999 <18054486+pt9999@users.noreply.github.com>
2024-10-11 00:29:56 +0900

    _projects: add _gendoc.sh

---
+++ _projects/_gendoc.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -e
+
+function gendoc () {
+    local dir=$1
+    (
+        cd $dir
+        if [ ! -d lib ]; then return; fi
+        local files=$(find lib -name "*.fix")
+        local modules=$(perl -lne 'print "$1" if /^module\s+([\w\.]+);/' $files)
+        echo "=== $dir ==="
+        #echo dir=$dir files=$files modules=$modules
+        fix docs -m $modules
+        rm -rf doc
+        mkdir -p doc
+        mv -v Minilib.*.md doc
+    )
+}
+
+for dir in fixlang-minilib-*; do
+    gendoc $dir
+done

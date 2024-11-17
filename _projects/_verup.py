1efbf80addc694ef92450c4ad3d736abad0e3f22
pt9999 <18054486+pt9999@users.noreply.github.com>
2024-11-17 12:34:04 +0900

    update _verup.py

--- _projects/_verup.py
+++ _projects/_verup.py
@@ -4,6 +4,8 @@ import sys
 import os
 import re
 import argparse
+from contextlib import contextmanager
+import subprocess
 
 def increment_patch_version(version: str) -> str:
     major, minor, patch = version.split(".", 3)
@@ -16,47 +18,55 @@ def increment_patch_version(version: str) -> str:
     patch = str(int(patch) + 1)
     return f"{major}.{minor}.{patch}"
 
-def update_fixproj_toml(args, project_dir, input_lines: list[str]) -> list[str]:
-    output_lines = input_lines
-    for i, line in enumerate(output_lines):
-        if "=" not in line:
-            continue
-        key, value = line.split("=", 1)
-        key = key.strip()
-        value = value.strip()
-        if key != "version":
-            continue
-        version = value.replace("\"", "")
-        version = increment_patch_version(version)
-        line = f"{key} = \"{version}\"\n"
-        output_lines[i] = line
-        break
-    return output_lines
+def commit_file(filepath: str, message: str):
+    subprocess.run(["git", "add", "--verbose", filepath], check=True)
+    subprocess.run(["git", "commit", "--verbose", "-m", message], check=True)
 
-def update_project(args, project_dir):
-    fixproj_path = f"{project_dir}/fixproj.toml"
-    if not os.access(fixproj_path, os.R_OK):
-        print("WARNING: file not found: " + fixproj_path)
+@contextmanager
+def update_file(filepath: str):
+    if not os.access(filepath, os.R_OK):
+        print("WARNING: file not found: " + filepath)
         return
-    with open(fixproj_path, "r") as f:
-        input_lines = list(f)
-    output_lines = update_fixproj_toml(args, project_dir, input_lines)
-    with open(fixproj_path + ".new", "w") as f:
-        f.write("".join(output_lines))
-    os.rename(fixproj_path, fixproj_path + ".bak")
-    os.rename(fixproj_path + ".new", fixproj_path)
-    print("Updated " + fixproj_path)
+    with open(filepath, "r") as input:
+        with open(filepath + ".new", "w") as output:
+            yield (input, output)
+    os.rename(filepath, filepath + ".bak")
+    os.rename(filepath + ".new", filepath)
+    print("Updated " + filepath)
+
+def update_project_file(args, project_dir):
+    fixproj_path = f"{project_dir}/fixproj.toml"
+    with update_file(fixproj_path) as (input, output):
+        input_lines = list(input)
+        output_lines = input_lines
+        version = ''
+        for i, line in enumerate(output_lines):
+            if "=" not in line:
+                continue
+            key, value = line.split("=", 1)
+            key = key.strip()
+            value = value.strip()
+            if key != "version":
+                continue
+            version = value.replace("\"", "")
+            version = increment_patch_version(version)
+            line = f"{key} = \"{version}\"\n"
+            output_lines[i] = line
+            break
+        output.write("".join(output_lines))
+    if args.commit and version != '':
+        commit_file(fixproj_path, 'version ' + version)
 
 def parse_args():
     parser = argparse.ArgumentParser(
                     prog='verup')
     parser.add_argument('projects', nargs='*', help='target projects to change', default=['fixlang-minilib-common'])
+    parser.add_argument('--commit', action='store_true', help='commit', default=False)
     args = parser.parse_args()
     return args
-    
 
 if __name__ == "__main__":
     args = parse_args()
 
     for project_dir in args.projects:
-        update_project(args, project_dir)
+        update_project_file(args, project_dir)

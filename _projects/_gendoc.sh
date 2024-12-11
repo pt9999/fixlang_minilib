6112af3b1babbb5991595ab97b5dd5cacbe32f2e
pt9999 <18054486+pt9999@users.noreply.github.com>
2024-12-11 17:44:55 +0900

    _gendoc.sh: bugfix: ignore .fixlang in `lib` directory
    Rational: if you accidentally type `fix test` in `lib` directory,
    `.fixlang` directory is created that contains source files of Std module,
    so `Std.md` will be created by `_gendoc.sh`.

--- _projects/_gendoc.sh
+++ _projects/_gendoc.sh
@@ -7,7 +7,7 @@ function gendoc () {
     (
         cd $dir
         if [ ! -d lib ]; then return; fi
-        local files=$(find lib -name "*.fix")
+        local files=$(find lib -name ".fixlang" -prune -o -name "*.fix")
         local modules=$(perl -lne 'print "$1" if /^module\s+([\w\.]+);/' $files)
         echo "=== $dir ==="
         #echo dir=$dir files=$files modules=$modules

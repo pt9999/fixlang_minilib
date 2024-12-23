8be3fa14098f0507c0694c26f39368c267c905f7
pt9999 <18054486+pt9999@users.noreply.github.com>
2024-12-23 20:31:40 +0900

    _sandbox: terminal: update ncurses (wip)

--- _sandbox/terminal/ncurses/lib.c
+++ _sandbox/terminal/ncurses/lib.c
@@ -13,7 +13,7 @@ void handle_fatal_signal(int signum)
     //char newlines[]="\r\n\r\n";
     //write(1, newlines, strlen(newlines));
 
-    if (true) {
+    if (false) {
         endwin();       // finalize screen, but assertion-failed messages are also erased
     } else {
         noraw();
@@ -44,6 +44,7 @@ void minilib_ncurses_initialize()
 
     // initialize colors
     start_color();
+    //init_pair(0, COLOR_BLACK, COLOR_BLACK); // cannot redefine
     init_pair(1, COLOR_RED, COLOR_BLACK);
     init_pair(2, COLOR_GREEN, COLOR_BLACK);
     init_pair(3, COLOR_YELLOW, COLOR_BLACK);
@@ -51,7 +52,6 @@ void minilib_ncurses_initialize()
     init_pair(5, COLOR_MAGENTA, COLOR_BLACK);
     init_pair(6, COLOR_CYAN, COLOR_BLACK);
     init_pair(7, COLOR_WHITE, COLOR_BLACK);
-    init_pair(10, COLOR_WHITE, COLOR_BLUE);
 }
 
 void minilib_ncurses_finialize()
@@ -59,7 +59,21 @@ void minilib_ncurses_finialize()
     endwin();       // finalize screen
 }
 
-WINDOW* minilib_ncurses_get_default_window()
+WINDOW* minilib_ncurses_get_standard_window()
 {
     return stdscr;
+}
+
+int minilib_ncurses_get_window_size(WINDOW* win)
+{
+    int w, h;
+    // NOTE getmaxyx() is a macro, so w and h are not passed as pointers
+    // and it always succeeds
+    getmaxyx(win, h, w);
+    return (w & 0xffff) << 16 | (h & 0xffff);
+}
+
+int minilib_ncurses_draw_border(WINDOW* win)
+{
+    return box(win, ACS_VLINE, ACS_HLINE);
 }

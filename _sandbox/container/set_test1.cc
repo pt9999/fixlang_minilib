b3bedf38df5cb1c87e19c85d2d27fd4e5b083e19
pt9999 <18054486+pt9999@users.noreply.github.com>
2025-11-03 02:56:19 +0900

    update pattree (wip)

--- _sandbox/container/set_test1.cc
+++ _sandbox/container/set_test1.cc
@@ -2,7 +2,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main()
+int main1()
 {
     int n = 200000;
     int mod = 100000;
@@ -12,4 +12,20 @@ int main()
         s.erase(rand() % mod);
     }
     cout << "s.size=" << s.size() << endl;
+    return 0;
 }
+
+int main()
+{
+    using ll = long long;
+
+    ll n = (ll) 1e6;
+    ll mod = 998244353;
+    set<ll> s;
+    for(ll i = 0; i < n; i++) {
+        s.insert((i * 12345) % mod);
+    }
+    cout << "s.size=" << s.size() << endl;
+    return 0;
+}
+
